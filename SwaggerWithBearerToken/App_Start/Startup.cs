9ce47323d7cfb031ea881c64a6bcfa59ce1a96e3
Liu <rliu3@activenetwork.com>
2018-04-26 16:24:33 +0800

    init commit

---
+++ SwaggerWithBearerToken/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(SwaggerWithBearerToken.App_Start.Startup))]
+namespace SwaggerWithBearerToken.App_Start
+{
+    public class Startup
+    {
+        public static OAuthBearerAuthenticationOptions OAuthBearerOptions = new OAuthBearerAuthenticationOptions();
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseOAuthBearerAuthentication(OAuthBearerOptions);
+        }
+    }
+}
---
+++ SwaggerWithBearerToken/Controllers/TokenController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.Owin.Security.OAuth;
+using SwaggerWithBearerToken.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace SwaggerWithBearerToken.Controllers
+{
+    public class TokenController : ApiController
+    {
+        public string Get()
+        {
+            var identity = new ClaimsIdentity("Bearer");
+            identity.AddClaim(new Claim(ClaimTypes.Name, "Roy"));
+
+            var ticket = new AuthenticationTicket(identity, new AuthenticationProperties());
+
+            var tokenFormat = Startup.OAuthBearerOptions.AccessTokenFormat;
+
+            var accessToken = tokenFormat.Protect(ticket);
+
+            return accessToken;
+        }
+    }
+}
